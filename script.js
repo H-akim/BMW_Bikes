85dcff77c700bc72423a5eb2cc65195587015f9c
H-akim <92632018+H-akim@users.noreply.github.com>
2023-09-18 13:17:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var bike = document.getElementById("bike");
+
+var red = document.getElementById("red").addEventListener("click", ()=>{
+    bike.style.background = "url(images/BMW1.png)";
+    bike.style.backgroundSize = "cover";
+    bike.style.backgroundPosition = "center";
+})
+var blue = document.getElementById("blue").addEventListener("click", ()=>{
+    bike.style.background = "url(images/BMW2.png)";
+    bike.style.backgroundSize = "cover";
+    bike.style.backgroundPosition = "center";
+})
+var black = document.getElementById("black").addEventListener("click", ()=>{
+    bike.style.background = "url(images/BMW3.png)";
+    bike.style.backgroundSize = "cover";
+    bike.style.backgroundPosition = "center";
+})
+
+
+
+
+
+
+
+
+
